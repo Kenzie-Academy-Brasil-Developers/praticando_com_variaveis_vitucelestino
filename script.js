399da8c2ab827631004f98166c7d7b250fc40037
vitucelestino <victorcelestino13@gmail.com>
2022-12-09 16:33:39 -0300

    'update'

---
+++ script.js
@@ -0,0 +1,77 @@
+// EXERCICIO 1
+
+// let dinheiroGasolina = prompt('Quanto em dinheiro você deseja por de gasolina?');
+// let valorLitro = 6.80;
+// let litros = dinheiroGasolina / valorLitro;
+// alert(`O cliente abasteceu: ${litros} litros`);
+
+// EXERCICIO 2
+
+// let Celsius = prompt('Digite a temperatura em Celsius');
+// let Fahrenheit = (9 * Celsius + 160) / 5;
+// alert(`O valor ${Celsius}ºc em Fahrenheit é ${Fahrenheit}`);
+
+// EXERCICIO 3
+
+// let Fahrenheit = prompt('Digite a temperatura em Fahrenheit');
+// let Celsius = ((Fahrenheit - 32) * 5) / 9;
+// alert(`O valor ${Fahrenheit}ºf em Celsius é ${Celsius}`);
+
+// EXERCICIO 4
+
+// let comprimento = prompt('Digite o comprimento');
+// let largura = prompt('Digite a largura');
+// let altura = prompt('Digite a altura');
+// let metros = comprimento * largura * altura;
+// alert(`O volume é de: ${metros}m³.`);
+
+// EXERCICIO 5
+
+// let valornumerico = prompt('Digite aqui o valor a ser elevado ao quadrado');
+// let resultado = valornumerico * valornumerico;
+// alert(`O valor ${valornumerico} ao quadrado é ${resultado}.`);
+
+// EXERCICIO 6
+
+// let nota1 = parseInt(prompt('Digite a 1ª nota aqui'));
+// let nota2 = parseInt(prompt('Digite a 2ª nota aqui'));
+// let nota3 = parseInt(prompt('Digite a 3ª nota aqui'));
+// let nota4 = parseInt(prompt('Digite a 4ª nota aqui'));
+// let nota5 = parseInt(prompt('Digite a 5ª nota aqui'));
+// let soma = nota1 + nota2 + nota3 + nota4 + nota5;
+// let media = soma / 5;
+// alert(`A soma dos valores infomados é: ${soma} e a média das notas é: ${media}`);
+
+// // EXERCICIO 7
+
+// let valor1 = prompt('Digite o primeiro valor para dividir');
+// let valor2 = prompt('Digite o segundo valor para dividir');
+
+// alert(`O resto da divisão de ${valor1} por ${valor2} é: ${valor1 % valor2}`)
+
+// EXERCICIO 8
+
+// let texto = prompt('Digite o seu texto aqui');
+// let quantidade = texto.length;
+// alert(`Este texto tem ${quantidade} caracteres`)
+
+// EXERCICIO 9
+
+// let texto = prompt('Digite seu texto aqui');
+
+// let caixaalta = texto.toUpperCase()[0];
+// let caixabaixa = texto.toLowerCase()[texto.length - 1];
+
+// alert(`A primeira letra deste texto é ${caixaalta} e útlima é a ${caixabaixa}`);
+
+// // EXERCICIO 10
+
+// let nome = prompt('Digite o seu nome');
+// let sobrenome = prompt('Digite o seu sobrenome');
+// let idade = prompt('Digite a sua idade');
+// let setor = prompt('Digite o seu setor');
+
+// let nome1 = nome.toUpperCase();
+// let sobrenome2 = sobrenome.toUpperCase();
+
+// alert(`nome: ${nome1} ${sobrenome2} - idade ${idade} - Setor: ${setor}`);
